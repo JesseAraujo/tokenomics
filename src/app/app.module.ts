968166fdbf66fc1a203cf1654fcc7c86e7dab36e
Jessé Brisola de Araujo <28305012+JesseAraujo@users.noreply.github.com>
2023-05-18 23:37:52 -0300

    add charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Only5OfBnbRaisedComponent } from './shared/only-5-of-bnb-raised/only-5-of-bnb-raised.component';
 import { FormsModule } from '@angular/forms';
+import {
+  CURRENCY_MASK_CONFIG,
+  CurrencyMaskConfig,
+  CurrencyMaskModule,
+} from 'ng2-currency-mask';
+
+export const CustomCurrencyMaskConfig: CurrencyMaskConfig = {
+  align: 'right',
+  allowNegative: true,
+  decimal: ',',
+  precision: 4,
+  prefix: '',
+  suffix: '',
+  thousands: '.',
+};
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Only5OfBnbRaisedComponent
+  declarations: [AppComponent, Only5OfBnbRaisedComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, CurrencyMaskModule],
+  providers: [
+    {
+      provide: CURRENCY_MASK_CONFIG,
+      useValue: CustomCurrencyMaskConfig,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/only-5-of-bnb-raised/only-5-of-bnb-raised.component.ts
+++ src/app/shared/only-5-of-bnb-raised/only-5-of-bnb-raised.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-only-5-of-bnb-raised',
@@ -8,7 +9,7 @@ import { Component } from '@angular/core';
     './only-5-of-bnb-raised.component.scss',
   ],
 })
-export class Only5OfBnbRaisedComponent {
+export class Only5OfBnbRaisedComponent implements OnInit {
   public totalSupply = 0;
   public hardcap = 0;
   public presaleRate = 0;
@@ -21,8 +22,17 @@ export class Only5OfBnbRaisedComponent {
   public youWillUseHowManyTotalSupply = 0;
   public totalBnbOwnerWallet = 0;
   public totalBnbPancakeSwap = 0;
+  public presaleLiquidity = 0;
+  public team = 0;
+  public listTokenomics: any = [];
+  public nameTokenomics: string = '';
+  public valueTokenomics = 0;
 
   isWork = 0;
+
+  ngOnInit() {
+    this.setListTokenomics();
+  }
 
   handleCalcTotalTokensForPresale() {
     this.totalTokensForPresale = this.presaleRate * this.hardcap;
@@ -48,6 +58,7 @@ export class Only5OfBnbRaisedComponent {
 
     this.calcIsWork();
     this.calcTotalBnb();
+    this.setListTokenomics();
   }
 
   calcIsWork() {
@@ -61,5 +72,147 @@ export class Only5OfBnbRaisedComponent {
       this.hardcap * (1 - this.liquidityPercentageOnPancake / 100) * 0.95;
     this.totalBnbPancakeSwap =
       this.hardcap * (this.liquidityPercentageOnPancake / 100) * 0.95;
+  }
+
+  setListTokenomics() {
+    this.listTokenomics = [
+      {
+        Name: 'Presale + Liquidity',
+        Value: this.youWillUseHowManyTotalSupply,
+        IsEditable: false,
+      },
+      {
+        Name: 'Team',
+        Value: 100 - this.youWillUseHowManyTotalSupply,
+        IsEditable: false,
+      },
+    ];
+
+    this.chartTotalSupplyPercentage(this.listTokenomics);
+    this.chartInToken(this.listTokenomics);
+  }
+
+  handleSetNewTokenomics(nameTokenomics: string, valueTokenomics: number) {
+    this.listTokenomics.push({
+      Name: nameTokenomics,
+      Value: valueTokenomics,
+      IsEditable: true,
+    });
+
+    this.listTokenomics
+      .filter((n) => n.Name === 'Team')
+      .map((element) => {
+        element.Value = element.Value - this.valueTokenomics;
+      });
+
+    this.nameTokenomics = '';
+    this.valueTokenomics = 0;
+
+    this.chartTotalSupplyPercentage(this.listTokenomics);
+    this.chartInToken(this.listTokenomics);
+  }
+
+  chartTotalSupplyPercentage(listTokenomics) {
+    let count = listTokenomics.length;
+
+    Highcharts.chart('chartTotalSupplyPercentage', {
+      chart: {
+        type: 'pie',
+        height: 300,
+        backgroundColor: '#00000000',
+      },
+      title: {
+        text: '',
+      },
+      tooltip: {},
+      accessibility: {
+        point: {
+          valueSuffix: '%',
+        },
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: false,
+          },
+        },
+      },
+      credits: {
+        enabled: false,
+      },
+      legend: {
+        enabled: false,
+      },
+      series: (function () {
+        var series: any = [];
+        var data: any = [];
+        for (var i = 0; i < count; i++) {
+          series.push({
+            name: listTokenomics[i].Name,
+            y: listTokenomics[i].Value,
+          });
+        }
+
+        data.push({
+          data: series,
+        });
+
+        return data;
+      })(),
+    });
+  }
+
+  chartInToken(listTokenomics) {
+    let count = listTokenomics.length;
+
+    Highcharts.chart('chartInToken', {
+      chart: {
+        type: 'bar',
+        height: 300,
+        backgroundColor: '#00000000',
+      },
+      title: {
+        text: '',
+      },
+      tooltip: {},
+      accessibility: {
+        point: {
+          valueSuffix: '%',
+        },
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: false,
+          },
+        },
+      },
+      credits: {
+        enabled: false,
+      },
+      legend: {
+        enabled: false,
+      },
+      series: (function () {
+        var series: any = [];
+        var data: any = [];
+        for (var i = 0; i < count; i++) {
+          series.push({
+            name: listTokenomics[i].Name,
+            y: listTokenomics[i].Value,
+          });
+        }
+
+        data.push({
+          data: series,
+        });
+
+        return data;
+      })(),
+    });
   }
 }
